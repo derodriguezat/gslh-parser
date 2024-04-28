ad819661b269f639ff4447dd86e71f78dc483175
Daniel Rodriguez Atara <rodriguezhm@gmail.com>
2024-04-28 21:18:46 +0200

    Add cli support

--- src/GslhParser/Program.cs
+++ src/GslhParser/Program.cs
@@ -1,51 +1,94 @@
-﻿using GslhParser;
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using GslhParser;
 
-Console.WriteLine("Hello, This is the Semantic Location History Parser!");
-Console.WriteLine("Please enter the path to the SLH");
+// Define the root command.
+var rootCommand = new RootCommand("Google Semantic Location History Parser");
 
-// Read the path to the SLH and check if it is valid
-var pathToSlh = Console.ReadLine();
-if (!Directory.Exists(pathToSlh))
+// Path argument.
+var pathArgument = new Argument<string>(
+    name: "path",
+    description: "The path to the SLH",
+    parse: (parseArgument) =>
 {
-    Console.WriteLine("The path is invalid. Please try again.");
-    return;
-}
+    if (!Directory.Exists(parseArgument.Tokens[0].Value))
+    {
+        parseArgument.ErrorMessage = "The path is invalid. Please try again.";
+        return default;
+    }
 
-var slhRoot = new SlhRoot(pathToSlh);
-Console.WriteLine($"The following years have been found in your SLH:\n {string.Join(',', slhRoot.GetSlhYears().Select(y => y.Year))}.");
-
-// Read the year from the user
-Console.WriteLine("Please enter the year you want to analyze.");
-var year = Console.ReadLine();
-if (!int.TryParse(year, out var yearInt))
+    return parseArgument.Tokens[0].Value;
+})
 {
-    Console.WriteLine("The year is invalid. Please try again.");
-    return;
-}
+    Arity = ArgumentArity.ExactlyOne,
+};
 
-if (!slhRoot.TryGetYear(yearInt, out var slhYear))
+rootCommand.AddArgument(pathArgument);
+
+// Workdays command.
+var workdaysCommand = new Command("workdays", "Get the number of workdays in a year and month");
+rootCommand.AddCommand(workdaysCommand);
+
+// Year options.
+var yearOption = new Argument<int>(
+    name: "year",
+    description: "The year you want to analyze",
+    parse: (argResult) =>
+    {
+        if (!int.TryParse(argResult.Tokens[0].Value, out var year) || year < 2010 || year > DateTime.Now.Year)
+        {
+            argResult.ErrorMessage = $"The year is invalid. It should be a value between 2010 and {DateTime.Now.Year}";
+            return default;
+        }
+
+        return year;
+    })
 {
-    Console.WriteLine($"Year {yearInt} not found.");
-    return;
-}
+    Arity = ArgumentArity.ExactlyOne
+};
 
-Console.WriteLine($"The following months have been found in your SLH for the year {yearInt}:\n {string.Join(',', slhYear!.GetSlhMonths().OrderBy(m => m.Month).Select(m => m.Month))}.");
+// Month option.
+var monthOption = new Option<int>(
+    name: "--month",
+    description: "The month you want to analyze", parseArgument: (argResult) =>
+    {
+        if (!int.TryParse(argResult.Tokens[0].Value, out var month) || month < 1 || month > 12)
+        {
+            argResult.ErrorMessage = "The month is invalid. It should be a value between 1 and 12";
+            return default;
+        }
 
-// Read the month from the user
-Console.WriteLine("Please enter the num ber of month you want to analyze.");
-var month = Console.ReadLine();
-if (!int.TryParse(month, out var monthInt))
+        return month;
+    })
 {
-    Console.WriteLine("The month is invalid. Please try again.");
-    return;
-}
+    Arity = ArgumentArity.ZeroOrOne
+};
 
-if (!slhYear.TryGetMonth(monthInt, out var slhMonth))
+workdaysCommand.AddArgument(yearOption);
+workdaysCommand.AddOption(monthOption);
+workdaysCommand.SetHandler((path, year, month) =>
 {
-    Console.WriteLine($"Month {monthInt} not found.");
-    return;
-}
+    var slhRoot = new SlhRoot(path);
+    if (!slhRoot.TryGetYear(year, out var slhYear))
+    {
+        Console.WriteLine($"Year {year} was not found.");
+        return;
+    }
 
-var workVisits = slhMonth!.GetWorkVisits();
-Console.WriteLine($"Work days on year {yearInt} : {workVisits.Count()}");
-Console.ReadLine();
+    if (month != 0)
+    {
+        if (!slhYear!.TryGetMonth(month, out var slhMonth))
+        {
+            Console.WriteLine($"Month {month} not found.");
+            return;
+        }
+
+        Console.WriteLine($"The number of workdays in {year}-{month} was {slhMonth!.GetWorkVisits().Count()}");
+        return;
+    }
+
+    var workVisits = slhYear!.GetSlhMonths().SelectMany(m => m.GetWorkVisits());
+    Console.WriteLine($"The number of workdays in {year} is {workVisits.Count()}");
+}, pathArgument, yearOption, monthOption);
+
+await rootCommand.InvokeAsync(args);
